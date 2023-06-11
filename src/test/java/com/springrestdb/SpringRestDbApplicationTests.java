84a872cf43afc14a726f53befeeba9b8750039a1
Saikumar Reddy Raju <sr19adr@herts.ac.uk>
2023-06-11 16:57:27 +0100

    First Commit

---
+++ src/test/java/com/springrestdb/SpringRestDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrestdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
