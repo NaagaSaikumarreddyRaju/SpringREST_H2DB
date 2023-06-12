da28fdf4cf23416f10fc69124aff20fda925f57e
Saikumar Reddy Raju <sr19adr@herts.ac.uk>
2023-06-12 13:31:34 +0100

    Delete,PUT Commit

--- src/main/java/com/springrestdb/UserController.java
+++ src/main/java/com/springrestdb/UserController.java
@@ -2,14 +2,12 @@ package com.springrestdb;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class UserController {
     @Autowired
     UserService userService;
@@ -27,5 +25,16 @@ public class UserController {
     @GetMapping("/getUserDetails")
     public List<User> getUserDetails(){
         return userService.getUserDetails();
+    }
+
+    @DeleteMapping("/delUserDetailsById/{id}")
+    public String delUserDetailsById(@PathVariable("id") Long id){
+        userService.delUserDetailsById(id);
+        return "Data Deleted Succesfully";
+    }
+
+    @PutMapping("/updateUserDetailsById/{id}")
+    public User updateUserDetailsById( @PathVariable("id") Long id, @RequestBody User user){
+        return userService.updateUserDetailsById(id,user);
     }
 }
--- src/main/java/com/springrestdb/UserService.java
+++ src/main/java/com/springrestdb/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,5 +22,20 @@ public class UserService {
 
     public List<User> getUserDetails() {
         return userRepository.findAll();
+    }
+
+    public void delUserDetailsById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUserDetailsById(Long id,User user) {
+
+       User olduser = userRepository.findById(id).get();
+
+        olduser.setFirstName(user.getFirstName());
+        olduser.setLastName(user.getLastName());
+        olduser.setEmail(olduser.getEmail());
+
+        return userRepository.save(olduser);
     }
 }
